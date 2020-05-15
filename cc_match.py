d65ecf7611444dcd3e7b684457367703a27d18df
mjordan <michael@glowingpixel.com>
2020-05-15 00:33:52 -0700

    Idenity by VFX ID only

--- cc_match.py
+++ cc_match.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
-import pathlib, sys
+import pathlib, sys, re
 import upco_filesequence
 
 if __name__ == "__main__":
 
-	usage = f"Usage: {__file__} /path/to/cc/folder/ /path/to/shots/folder/ [...]"
+	usage = f"Usage: {__file__} /path/to/cc/folder/ [/path/to/shots/folder/] [...]"
+
+	vendor_names  = {"WTA":"Weta Digital"}
+	path_template = pathlib.Path(__file__).parent / "template.txt"
+
 
 	glob_cc  = []
 	path_inputs = []
 
-	path_template = pathlib.Path(__file__).parent / "template.txt"
+	
 	
 	if len(sys.argv) < 2:
 		sys.stderr.write(f"{usage}\n")
@@ -45,17 +49,30 @@ if __name__ == "__main__":
 	success = {}
 
 	# Find corresponding EXR sequences based on CC named + _EXR
+
+	pat_vfx_id = re.compile(r"^\d{3}[a-z]?_([a-z]{3})_\d{4}", re.I)
+	pat_vendor_code = re.compile(r".+_COMP_(?P<vendor_code>[a-z]{3})", re.I)
+
 	for file_cc in glob_cc:
-		shot_name = f"{file_cc.stem}_EXR"
+
+		# Verify .cc file begins with a VFX ID
+		match = pat_vfx_id.match(file_cc.stem)
+		if not match:
+			failed.update({file_cc: "CC filename does not start with a VFX ID."})
+			continue
+
+		vfx_id = match.group(0)
+
+#		shot_name = f"{file_cc.stem}_EXR"
 		glob_exr = []
-		{glob_exr.extend(path.rglob(f"{shot_name}/")) for path in path_inputs}
+		{glob_exr.extend(path.rglob(f"{vfx_id}*/")) for path in path_inputs}
 		
 		# Fail if no shots found, or more than one
 		if len(glob_exr) != 1:
 			failed.update({file_cc: "Multiple EXR sequences found." if len(glob_exr) else "No matching EXR sequences found."})
 			continue
 		
-		seq_exr = upco_filesequence.Sequencer(glob_exr[0].glob(f"{shot_name}*.[eE][xX][rR]"))
+		seq_exr = upco_filesequence.Sequencer(glob_exr[0].glob(f"{vfx_id}*.[eE][xX][rR]"))
 
 		# If the sequencer detects more than one EXR sequence, or none
 		if len(seq_exr.sequences) != 1:
@@ -63,6 +80,12 @@ if __name__ == "__main__":
 			continue
 
 		seq_exr = seq_exr.sequences[0]
+
+		match = pat_vendor_code(seq_exr.parent.name)
+		if not match:
+			failed.update({file_cc: "No vendor code found in EXR folder name."})
+
+		vendor_name = vendor_names.get(match.group("vendor_code").upper(), match.group("vendor_code").upper())
 		
 		sys.stdout.write(f"{file_cc.name}\t-> {pathlib.PurePath(seq_exr.grouped())}\n")
 		success.update({file_cc: {
@@ -70,8 +93,9 @@ if __name__ == "__main__":
 			"frame_last": seq_exr.max,
 			"source_seq": str(pathlib.Path(seq_exr.parent, f"{seq_exr.basename}%{str(seq_exr.padding).zfill(2)}d{seq_exr.ext}").resolve(strict=False)).replace('\\', '/'),
 			"source_cc": str(file_cc.resolve(strict=False)).replace('\\','/'),
-			"shot_name": file_cc.stem,
-			"prores_file": str(pathlib.Path("D:","Output","prepped_for_studio", seq_exr.parent.parent, f"{shot_name}_prores444.mov").resolve(strict=False)).replace('\\','/')
+			"shot_name": seq_exr.parent.name,
+			"vendor_name": vendor_name,
+			"prores_file": str(pathlib.Path("D:","Output","prepped_for_studio", seq_exr.parent.parent, f"{seq_exr.parent.name}_prores4444.mov").resolve(strict=False)).replace('\\','/')
 		}})
 
 	# Make sure nothing crazy happened
